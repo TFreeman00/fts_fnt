0e020d698c43954db83390937c66d38bb2c732e7
ooMatter <Tyrice02@gmail.com>
2024-07-08 09:58:51 -0400

    added posts middleware to store

--- api/store.js
+++ api/store.js
@@ -3,16 +3,24 @@ import { usersApi } from "./usersApi";
 import { authApi } from "./authApi";
 import usersSlice from "../slice/usersSlice";
 import authSlice from "../slice/authSlice";
+import { postsApi } from "./postsApi";
+import postsSlice from "../slice/postsSlice";
 
 const store = configureStore({
   reducer: {
     authSlice,
     usersSlice,
+    postsSlice,
     [authApi.reducerPath]: authApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
+    [postsApi.reducerPath]: postsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, usersApi.middleware),
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      usersApi.middleware,
+      postsApi.middleware
+    ),
 });
 
 export default store;
